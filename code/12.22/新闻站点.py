c5fed6d719e2d19b2982e3755a19499d9c7c81c2
袁鑫攀 <xpyuanfly@163.com>
2020-12-24 19:07:24 +0800

    u

--- code/12.22/新闻站点.py
+++ code/12.22/新闻站点.py
@@ -1,89 +1,98 @@
-class news:
-    def __init__(self,id,title,time,link):
-        self.id =id
-        self.title=title
-        self.time = time
-        self.link = link
-    def setContent(self,content):
-        self.content = content
-    def __repr__(self):
-        return "{}-{}".format(self.id,self.title)
-    
-#step1 : 读取磁盘中的所有新闻
-news_ls=[]
-dir_path = 'c:\\news\\'
+from flask import Flask, redirect, url_for, request
 import os
-files = os.listdir(dir_path)
-for filename in files:
-    id= filename[0:3]
-    title = filename.split('.')[0][3:]
-    with open(dir_path+filename,'r',encoding='utf-8') as f_obj:
-        filecontent = f_obj.read()
-    # print(filecontent)
-    new = news(id,title,None,None)
-    new.setContent(filecontent)
-    # print(n)
-    news_ls.append(new)
 
-#step2 : 启动站点
-from flask import Flask,request,escape,redirect,url_for
+
+class news:
+    def __init__(self, id, title):
+        self.id = id
+        self.title = title
+
+    def setContent(self, content):
+        self.content = content
+
+
 app = Flask(__name__)
+
+# step1 : 获取全部新闻
+ls_news = []
+dir_path = 'c:\\news\\'
+ls_filename = os.listdir(dir_path)
+for filename in ls_filename:
+    id = filename[0:3]
+    title = filename[3:-4]
+    with open(dir_path+filename, 'r', encoding='utf-8') as f_obj:
+        str_content = f_obj.read()
+    new = news(id, title)
+    new.setContent(str_content)
+    ls_news.append(new)
+
+# print(ls_news)
+
 
 @app.route('/')
 def showAllNews():
-    res = ''
-    # str_news_ls = list(map(lambda x: '<a href = {}> {}</a>'.format(x.link ,x.__repr__()),news_ls))
-    for item in news_ls:
-        res+= '<a href="/{}/show">{}</a> <a href="/{}/del">【删除】</a>|<a href="/{}/edit">【修改】</a><br>'.format(item.id,item.id+item.title,item.id,item.id)
-    
-    return '<a>添加</a><br>'+res
+    res = '<a href="/add">添加新闻</a><br>'
+    for item in ls_news:
+        res += '{}-<a href ="/{}/show">{}</a>---<a href="/{}/del">删除</a>--<a href="/{}/edit">修改</a><br>'.format(
+            item.id, item.id, item.title, item.id, item.id)
+    return res
+
 
 @app.route('/<id>/show')
 def showOneNews(id):
-    res = ''
-    for item in news_ls:
-        if item.id == escape(id):
-            res+= item.content.replace("\n",'<br>')
-    print(res)        
-    return res
+    for i in range(0, len(ls_news)):
+        if ls_news[i].id == id:
+            return ls_news[i].content.replace('\n', '<br>')
+            break
+
 
 @app.route('/<id>/del')
-def delOneNews(id):
-    res = '<alert>删除成功</alert>'
-    for i in range(0,len(news_ls)):
-        if news_ls[i].id == escape(id):
-            news_ls.pop(i)
+def deleteOneNews(id):
+    for i in range(0, len(ls_news)):
+        if ls_news[i].id == id:
+            ls_news.pop(i)
             break
-   
-    return redirect(url_for('showAllNews'),code=302)
+    return redirect(url_for("showAllNews"), code=302)
+
 
 @app.route('/<id>/edit')
 def editOneNews(id):
-    for i in range(0,len(news_ls)):
-        if news_ls[i].id == escape(id):
-            news=news_ls[i]
+    res = ''
+    for item in ls_news:
+        if item.id == id:
+            res += '<form action="/{}/update" method="POST">\
+                    标题:<input type="text" name = "title" value="{}" style="width:550px"/>\
+                    <input type="submit" value="修改" />\
+                        </form>'.format(item.id,item.title)
             break
-     
-    res = '\
-    <form action="/{}/update" method="POST">\
-    标题：<br>\
-    <input type="text" name="title" style="width:550px" value="{}">\
-    <br>\
-    <input type="submit" value="修改">\
-    </form>'.format(news.id,news.title)
-    print(res)
     return res
 
-@app.route('/<id>/update', methods=['POST'])
+@app.route('/<id>/update',methods=['POST'])
 def updateOneNews(id):
     if request.method == 'POST':
-        # print(id)        
         title = request.form.get('title')
-        # print(title)
-        for i in range(0,len(news_ls)):
-            if news_ls[i].id == escape(id):
-                news_ls[i].title= title
+        for item in ls_news:
+            if item.id == id:
+                item.title = title
                 break
-        return redirect(url_for('showAllNews'),code=302)
+    return redirect(url_for('showAllNews'),code=302)
 
-app.run(port=5000, debug=True)
+@app.route('/add')
+def addOneNews():
+    res =  '<form action="/addsubmit" method="POST">\
+                    id:<input type="text" name = "id" value="" /><br>\
+                    标题:<input type="text" name = "title" value="" style="width:550px"/><br>\
+                    正文:<textarea name = "content" value="" ></textarea><br>\
+                    <input type="submit" value="添加" />\
+            </form>'
+    return res
+
+@app.route('/addsubmit',methods=['POST'])
+def addsubmitOneNews():
+    print('11111')
+    n = news(request.form.get('id'),request.form.get('title'))
+    n.setContent(request.form.get('content'))
+    ls_news.append(n)
+    return redirect(url_for('showAllNews'),code=302)
+    
+app.run(debug=True)
